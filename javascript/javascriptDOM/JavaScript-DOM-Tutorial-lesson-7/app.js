73e8b0632a07357b4e840a06e49659e278fca1af
Tammy Laforest <TammyLaforest@gmail.com>
2018-08-13 02:20:46 +0000

    Lesson 7

---
+++ javascript/javascriptDOM/JavaScript-DOM-Tutorial-lesson-7/app.js
@@ -0,0 +1,32 @@
+// const bookList = document.querySelector('#book-list');
+
+// console.log('book list parent element:', bookList.parentElement);
+// console.log('book list parent node:', bookList.parentNode);
+
+// console.log('all node children:');
+// Array.from(bookList.childNodes).forEach(function(node){
+//   console.log(node);
+// });
+
+// console.log('all element children:');
+// Array.from(bookList.children).forEach(function(node){
+//   console.log(node);
+// });
+
+// const titles = bookList.querySelectorAll('.name');
+
+// console.log('Book titles:');
+// Array.from(titles).forEach(function(title){
+//   console.log(title.textContent);
+// });
+
+
+
+const bookList =document.querySelector('#book-list');
+
+console.log('the parent node is:', bookList.parentNode);
+console.log('book list parent element:', bookList.parentElement.parentElement);
+
+//console.log(bookList.childNodes)
+
+console.log(bookList.children)
