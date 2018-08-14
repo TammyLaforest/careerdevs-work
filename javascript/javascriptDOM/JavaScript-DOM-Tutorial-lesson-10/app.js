738b178b4d7b7746bc04631238a74dc5a853d843
Tammy Laforest <TammyLaforest@gmail.com>
2018-08-14 02:20:54 +0000

    lesson 10

---
+++ javascript/javascriptDOM/JavaScript-DOM-Tutorial-lesson-10/app.js
@@ -0,0 +1,19 @@
+// const list = document.querySelector('#book-list ul');
+
+// // delete books
+// list.addEventListener('click', (e) => {
+//   if(e.target.className == 'delete'){
+//     const li = e.target.parentElement;
+//     li.parentNode.removeChild(li);
+//   }
+// });
+
+const list = document.querySelector('#book-list ul');
+//delete books comment
+list.addEventListener('click', function(e){
+if(e.target.className == 'delete'){
+	const li = e.target.parentElement;
+	//li.parentNode.removeChild(li);
+	list.parentNode.removeChild(li);
+}
+})
