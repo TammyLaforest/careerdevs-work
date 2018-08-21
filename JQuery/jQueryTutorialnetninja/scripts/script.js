97ed8a2319e79a95673b0692cd36043d2d4dd63a
Tammy Laforest <TammyLaforest@gmail.com>
2018-08-21 02:34:57 +0000

    lesson5jquery

---
+++ JQuery/jQueryTutorialnetninja/scripts/script.js
@@ -0,0 +1,12 @@
+/*global $*/
+
+// $("#lead-banner p");
+
+
+$("h3").css({border:"3px solid blue"});
+
+$(".wrapper").css({border:"3px solid red"});
+
+$("#clients").css({border: "3px solid yellow"});
+
+
