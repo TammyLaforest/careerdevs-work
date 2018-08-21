4dee3863c81a3dd57ce8d0ec506eb9670c78c35d
Tammy Laforest <TammyLaforest@gmail.com>
2018-08-21 03:06:08 +0000

    lesson6jquery

--- JQuery/jQueryTutorialnetninja/scripts/script.js
+++ JQuery/jQueryTutorialnetninja/scripts/script.js
@@ -2,11 +2,35 @@
 
 // $("#lead-banner p");
 
+//element
+//$("h3").css({border:"3px solid blue"});
+//class
+////id
+//$("#clients").css({border: "3px solid yellow"});
 
-$("h3").css({border:"3px solid blue"});
+//filters
+//filters, first child last child
+// $("header nav li:first").css({border: "2px solid red"});
+// $("header nav li:last").css({border: "2px solid red"});
 
-$(".wrapper").css({border:"3px solid red"});
+//not first-child or last-child
+// $("#contact ul:first").css({border: "2px solid red"});
+// $("#contact ul:last").css({border: "2px solid red"});
 
-$("#clients").css({border: "3px solid yellow"});
 
+//filters, even odd
+// $("header nav li:even").css({border: "2px solid blue"});
+// $("header nav li:odd").css({border: "2px solid yellow"});
+// //filters, not
+// $("section:not('#contact')").css({border: "2px solid green"});
 
+// //filters, less than
+// $("#social-nav li:lt(3)").css({border: "2px solid pink"});
+//filters, greater than
+// $("#social-nav li:gt(1)").css({border: "2px solid blue"});
+
+// //filters, attribute
+// $("img[alt]").css({border: "2px solid pink"});
+
+// //filters, attribute with specific value
+// $("img[alt=quote]").css({border: "2px solid purple"});
