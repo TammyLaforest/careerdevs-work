cefaf53be811cda8e0cba76dbe183a862870796f
Tammy Laforest <TammyLaforest@gmail.com>
2018-08-21 03:48:33 +0000

    lesson7jquery

--- JQuery/jQueryTutorialnetninja/scripts/script.js
+++ JQuery/jQueryTutorialnetninja/scripts/script.js
@@ -1,6 +1,30 @@
 /*global $*/
 
-// $("#lead-banner p");
+// //next
+// $("#contact-methods").next().css({border: "3px solid red"});
+
+// //prev
+// $("#social-nav").prev().css({border: "3px solid red"});
+
+// //parent 
+// $(".banner-title").parents().css({border: "3px solid pink"});
+
+// //children
+// $("#social-nav").children().css({border: "3px solid green"});
+
+// //find
+// $("#contact").find(".facebook").css({border: "3px solid purple"});
+
+//closest
+// $("#social-nav").closest(".wrapper").css({border: "3px solid orange"});
+
+
+
+
+
+
+
+
 
 //element
 //$("h3").css({border:"3px solid blue"});
