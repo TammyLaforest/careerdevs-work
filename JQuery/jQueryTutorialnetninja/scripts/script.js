a9466f8f7852b3d2f2139177b8d98bda6179883c
Tammy Laforest <TammyLaforest@gmail.com>
2018-08-24 01:57:12 +0000

    lesson9jquery

--- JQuery/jQueryTutorialnetninja/scripts/script.js
+++ JQuery/jQueryTutorialnetninja/scripts/script.js
@@ -1,4 +1,22 @@
 /*global $*/
 
-$("#contact-methods").css({border: "2px solid red"}).next().css({border: "2px solid green"})
-.closest("section").css({border:"2px solid blue"});
+// var tweet = "<div style='margin:20px 0; padding: 10px; background: #eee'>The big fight live: Ham vs Cheese!</div>";
+var tweet = "The big fight live: Ham vs Cheese!";
+
+// $("#tweets div").append(tweet)
+
+// $("#tweets div").prepend(tweet)
+
+
+// $("#tweets div p").before(tweet)
+
+// $("#tweets div p").after(tweet)
+
+// $("#tweets div p").html(tweet)
+
+
+// $("#tweets div ").html(tweet) //replaces the html
+
+
+$("#tweets div p").text(tweet) //want text of p tag to be tweet
+
