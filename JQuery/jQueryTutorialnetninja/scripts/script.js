2f553ff5b1da1c40a9d412879063e1a66d48553e
Tammy Laforest <TammyLaforest@gmail.com>
2018-08-24 02:23:55 +0000

    lesson11jquery

--- JQuery/jQueryTutorialnetninja/scripts/script.js
+++ JQuery/jQueryTutorialnetninja/scripts/script.js
@@ -5,19 +5,27 @@
 // $("section").unwrap("");
 // $("section").wrapAll("<div>");
 
-var wrapper ="<div class='wrapper'>";
-var button = $(".button");
-var wrapped = true;
+// var wrapper ="<div class='wrapper'>";
+// var button = $(".button");
+// var wrapped = true;
 
-button[0].onclick = function(){
-    if(wrapped){
-        $("section").unwrap();
-        wrapped = false;
-        button.text("Wrap")
+// button[0].onclick = function(){
+//     if(wrapped){
+//         $("section").unwrap();
+//         wrapped = false;
+//         button.text("Wrap")
         
-    } else {
-        $("section").wrapAll(wrapper);
-        wrapped = true;
-        button.text("Unwrap")
-    }
-};
+//     } else {
+//         $("section").wrapAll(wrapper);
+//         wrapped = true;
+//         button.text("Unwrap")
+//     }
+// };
+
+$(".button").empty();
+$("#points-of-sale").empty();
+
+$(".button").remove();
+$("#contact img").remove();
+
+
