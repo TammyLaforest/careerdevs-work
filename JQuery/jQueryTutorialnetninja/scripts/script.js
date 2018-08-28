10d2cc88d8a4381dda63376d1042104317d6e317
Tammy Laforest <TammyLaforest@gmail.com>
2018-08-28 02:53:50 +0000

    lesson15jquery

--- JQuery/jQueryTutorialnetninja/scripts/script.js
+++ JQuery/jQueryTutorialnetninja/scripts/script.js
@@ -14,17 +14,19 @@
 // });
 
 
-
-
-
-
-
 // $("header .wrapper").removeClass("wrapper");
 // $("header > div").addClass("wrapper");
 
-var button = $("#lead-banner a");
-button[0].onclick = function() {
-  $("#points-of-sale").toggleClass("open");
-  return false;
+// var button = $("#lead-banner a");
+// button[0].onclick = function() {
+//   $("#points-of-sale").toggleClass("open");
+//   return false;
     
-};
+// };
+
+var myLis = $("#points-of-sale li");
+
+myLis.on("click", function(){
+    $(this).css({"background" : "pink"});
+    myLis.off("click");
+});
