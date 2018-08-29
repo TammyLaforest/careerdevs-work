71d129e144eb82d251f9133877a86140e9810906
Tammy Laforest <TammyLaforest@gmail.com>
2018-08-29 02:36:14 +0000

    lesson21jquery

--- JQuery/jQueryTutorialnetninja/scripts/script.js
+++ JQuery/jQueryTutorialnetninja/scripts/script.js
@@ -1,23 +1,11 @@
 /*global $*/
  $(document).ready(function(){
-    // function complete() {
-    //  alert ("Don't use alerts like this. It's amature.")
-    // } 
-    //  $("section > h2").on("click", function(){
-    //   $(this).animate({"width": "500px", "height" : "100px"}, 1000, "linear", complete);
-     
-     $("section > h2").on("click", function(){
-      // this.fadeOut(1000).fadeIn(1000);
-      
-      this.fadeTo(200, .2).
-           fadeTo(200,.8).
-     fadeTo(200, .2).
-           fadeTo(200,.8).
-     fadeTo(200, .2).
-           fadeTo(200,.8).
-     fadeTo(200, .2).
-           fadeTo(200,.8);
-     
+
+
+          $("img[alt=map]").on("click", function(){
+           
+           $("section > h2").toggle(1000);
+          
           
  });
      
