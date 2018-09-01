744b4aca392c5b3f0a77813646456302f1143762
Tammy Laforest <TammyLaforest@gmail.com>
2018-09-01 18:18:10 +0000

    lesson24jquery slide toggle

--- JQuery/jQueryTutorialnetninja/scripts/script.js
+++ JQuery/jQueryTutorialnetninja/scripts/script.js
@@ -1,25 +1,13 @@
 /*global $*/
  $(document).ready(function(){
 
-  var allQuotes = $("blockquote");
-  var currentQuote = 0;
-  
-  function changeQuote(){
+  var items = $("#points-of-sale li");
+  items.on("click", function(){
+   $(this).find("p").slideToggle(500);
    
-   $(allQuotes[currentQuote]).fadeOut(200, function(){
-     
-     if(currentQuote == allQuotes.length-1) {
-     currentQuote = 0;
-    } else {
-     currentQuote++;
-    }
-    
-    $(allQuotes[currentQuote]).fadeIn(200);
-  });
-   }
    
- var quoteTimer = setInterval(changeQuote, 3000);
-  
+   
+  })
  });
      
      
