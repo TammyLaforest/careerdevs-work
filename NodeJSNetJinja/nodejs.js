a7aa818d274621a3878455675aa212855ed96a22
Tammy Laforest <TammyLaforest@gmail.com>
2018-09-13 21:46:05 +0000

    setInterval

--- NodeJSNetJinja/nodejs.js
+++ NodeJSNetJinja/nodejs.js
@@ -1,7 +1,9 @@
 
+var time = 0;
 
-setTimeout(function(){
-    console.log('3 seconds have passed');
-},3000);
+setInterval(function(){
+    time += 2;
+    console.log(time + ' seconds have passed');
+},2000);
     
     
