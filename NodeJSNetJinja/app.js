a43192d09c1e298f946104f8067a726c2c0afe68
Tammy Laforest <TammyLaforest@gmail.com>
2018-09-14 03:04:46 +0000

    event 2

--- NodeJSNetJinja/app.js
+++ NodeJSNetJinja/app.js
@@ -1,9 +1,36 @@
 var events = require('events'); 
+var util = require('util'); 
 
-var myEmitter = new events.EventEmitter();
 
-myEmitter.on('someEvent', function(mssg){
-    console.log(mssg);
-});
+var Person =function(name){
+    this.name = name;
+    
+};
+    
+    util.inherits(Person, events.EventEmmitter);
+    
+    var james = new Person('james');
+        var mary = new Person('mary');
+            var ryu = new Person('ryu');
+    var people = [james, mary, ryu];
+    
+    people.forEach(function(person){
+       person.on('speak', function(mssg){
+           
+           console.log(person.name + ' said: ' + mssg);
+       }); 
+    });
+    
+    james.emit('speak', 'hey dudes');
 
-myEmitter.emit('someEvent', 'the event was emitted');
+
+
+// var events = require('events'); 
+
+// var myEmitter = new events.EventEmitter();
+
+// myEmitter.on('someEvent', function(mssg){
+//     console.log(mssg);
+// });
+
+// myEmitter.emit('someEvent', 'the event was emitted');
