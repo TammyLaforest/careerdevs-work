b612bec7b718209f1d2371c3dde43d4c0fef213b
Tammy Laforest <TammyLaforest@gmail.com>
2018-09-16 00:49:16 +0000

    read write stream

--- NodeJSNetJinja/app.js
+++ NodeJSNetJinja/app.js
@@ -1,11 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-
-myReadStream.on('data', function(chunk){ 
-    console.log('new chunk reveived: ');
-    console.log(chunk);
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
     
+myReadStream.on('data', function(chunk){ 
+console.log('new chunk recieved: ');
+  myWriteStream.write(chunk);
     
 });
 
