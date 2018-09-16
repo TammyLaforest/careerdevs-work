50b730518d594caadd318b9898a3169edc7f1eb4
Tammy Laforest <TammyLaforest@gmail.com>
2018-09-16 00:52:48 +0000

    pipe instead of read write stream longhand

--- NodeJSNetJinja/app.js
+++ NodeJSNetJinja/app.js
@@ -3,11 +3,18 @@ var fs = require('fs');
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
 var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
     
-myReadStream.on('data', function(chunk){ 
-console.log('new chunk recieved: ');
-  myWriteStream.write(chunk);
     
-});
+    //pipe can only be used on readable streams
+myReadStream.pipe(myWriteStream);
+
+
+
+
+// .on('data', function(chunk){ 
+// console.log('new chunk recieved: ');
+//   myWriteStream.write(chunk);
+    
+// });
 
 
 
