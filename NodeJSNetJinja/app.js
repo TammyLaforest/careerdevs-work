6110a0b69c0bc5922d1d2e81a62bd3babb8dfc0c
Tammy Laforest <TammyLaforest@gmail.com>
2018-09-16 00:55:50 +0000

    added read write into server with pipe

--- NodeJSNetJinja/app.js
+++ NodeJSNetJinja/app.js
@@ -1,34 +1,21 @@
 var http = require('http');
 var fs = require('fs');
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
-    
-    
+
+
+
+
+var server = http.createServer(function(req, res){
+  console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+
     //pipe can only be used on readable streams
-myReadStream.pipe(myWriteStream);
+myReadStream.pipe(res);
+    res.end('Hey, Ninjas.');
+});
 
-
-
-
-// .on('data', function(chunk){ 
-// console.log('new chunk recieved: ');
-//   myWriteStream.write(chunk);
-    
-// });
-
-
-
-
-// var http = require('http');
-
-// var server = http.createServer(function(req, res){
-   
-//   console.log('request was made: ' + req.url);
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('Hey, Ninjas.');
-// });
-
-
+// var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt'); 
+//replaced myWriteStream with res
 // server.listen(8080, '0.0.0.0' );
 
 // console.log('Now listening to port 8080');
@@ -37,3 +24,9 @@ myReadStream.pipe(myWriteStream);
 // // server.listen(8080, '127.0.0.1');
 
 // //control c cancels process
+
+// .on('data', function(chunk){ 
+// console.log('new chunk recieved: ');
+//   myWriteStream.write(chunk);
+    
+// });
