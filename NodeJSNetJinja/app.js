1c4ffdb23260936306a8033c23d23d3c16add8c3
Tammy Laforest <TammyLaforest@gmail.com>
2018-09-16 00:45:40 +0000

    how to make readable stream

--- NodeJSNetJinja/app.js
+++ NodeJSNetJinja/app.js
@@ -1,18 +1,32 @@
 var http = require('http');
+var fs = require('fs');
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
 
-var server = http.createServer(function(req, res){
-   
-   console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey, Ninjas.');
+myReadStream.on('data', function(chunk){ 
+    console.log('new chunk reveived: ');
+    console.log(chunk);
+    
+    
 });
 
 
-server.listen(8080, '0.0.0.0' );
-
-console.log('Now listening to port 8080');
 
 
-// server.listen(8080, '127.0.0.1');
+// var http = require('http');
 
-//control c cancels process
+// var server = http.createServer(function(req, res){
+   
+//   console.log('request was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hey, Ninjas.');
+// });
+
+
+// server.listen(8080, '0.0.0.0' );
+
+// console.log('Now listening to port 8080');
+
+
+// // server.listen(8080, '127.0.0.1');
+
+// //control c cancels process
