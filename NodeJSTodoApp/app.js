8598d9fb7e556279f2649d559777559533a82604
Tammy Laforest <TammyLaforest@gmail.com>
2018-09-17 13:35:18 +0000

    fixed everything with comment on lesson 34 by Geoffrey Hoffman

--- NodeJSTodoApp/app.js
+++ NodeJSTodoApp/app.js
@@ -21,29 +21,3 @@ console.log('You are listening to port 8080');
 
 
 
-//old stuff from tutorial for referencers
-
-// localhost:8080/assets/styles.css
-// var bodyParser = require('body-parser');
-// var urlencodedParser = bodyParser.urlencoded({ extended: false });
-// );
-// app.get('/', function(req, res){
-//     res.render('index');
-// });
-
-// app.get('/contact', function(req, res){
-   
-//     res.render('contact', {qs: req.query});
-    
-// });
-
-// app.post('/contact', urlencodedParser, function(req, res){
-//     console.log(req.body);
-//     res.render('contact-success', {data: req.body});
-    
-// });
-
-// app.get('/profile/:name', function(req, res){
-//     var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
-//     res.render('profile', {person: req.params.name, data: data});
-// });
--- NodeJSTodoApp/controllers/todoController.js
+++ NodeJSTodoApp/controllers/todoController.js
@@ -1,20 +1,22 @@
-var data = [{item: 'get milk'},{item: 'get eggs'},{item: 'get a million dollars'}, ]
+var bodyParser = require('body-parser');
 
+var data = [{item: 'get milk'},{item: 'get eggs'},{item: 'get a million dollars'} ];
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app){
+    
     app.get('/todo', function(req, res){
         res.render('todo', {todos: data});
         
     });
     
-      app.post('/todo', function(req, res){
-        
+      app.post('/todo', urlencodedParser, function(req, res){
+        data.push(req.body);
+        res.json({todos:data});
         
     });
     
       app.delete('/todo', function(req, res){
-        
-        
         
     });
     
