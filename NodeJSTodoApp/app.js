8c94336e54a15a14c210a89bf7472e8c0f7c3469
Tammy Laforest <TammyLaforest@gmail.com>
2018-09-17 12:59:51 +0000

    set up app.js for todo

--- NodeJSTodoApp/app.js
+++ NodeJSTodoApp/app.js
@@ -1,34 +1,46 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+//set up template engine 
 
 app.set('view engine', 'ejs');
-app.use('/assets', express.static('assets')
 
-);
+//static files
+app.use(express.static('./public'));
 
-app.get('/', function(req, res){
-    res.render('index');
-    
-});
+// localhost:8080/assets/styles.css
 
-app.get('/contact', function(req, res){
-   
-    res.render('contact', {qs: req.query});
-    
-});
-
-app.post('/contact', urlencodedParser, function(req, res){
-    console.log(req.body);
-    res.render('contact-success', {data: req.body});
-    
-});
-
-app.get('/profile/:name', function(req, res){
-    var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
-    res.render('profile', {person: req.params.name, data: data});
-});
+//listen to port
 
 app.listen(8080);
+console.log('You are listening to port 8080');
+
+
+
+// var bodyParser = require('body-parser');
+
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// );
+
+// app.get('/', function(req, res){
+//     res.render('index');
+    
+// });
+
+// app.get('/contact', function(req, res){
+   
+//     res.render('contact', {qs: req.query});
+    
+// });
+
+// app.post('/contact', urlencodedParser, function(req, res){
+//     console.log(req.body);
+//     res.render('contact-success', {data: req.body});
+    
+// });
+
+// app.get('/profile/:name', function(req, res){
+//     var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
+//     res.render('profile', {person: req.params.name, data: data});
+// });
