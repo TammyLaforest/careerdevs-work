c3921ac267555f85282d893552ca55ae37af09f2
Tammy Laforest <TammyLaforest@gmail.com>
2018-09-17 02:56:10 +0000

    query strings

--- NodeJSTutorial/app.js
+++ NodeJSTutorial/app.js
@@ -11,7 +11,8 @@ app.get('/', function(req, res){
     
 });
 app.get('/contact', function(req, res){
-    res.render('contact');
+    // console.log(req.query);
+    res.render('contact', {qs: req.query});
     
 });
 
